d8cc284541ed9afe946c9b552741854546de58f7
MishaDrog <drogmisha@gmail.com>
2022-12-08 17:42:29 +0200

    js_hw8

--- lessons8/tasc8/task8.js
+++ lessons8/tasc8/task8.js
@@ -20,13 +20,42 @@
 
     const filter = arrayUsers.filter(value => value.id % 2 === 0);
     console.log(filter);
+
+
 /*
 - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+*/
+function User(id, name, surname, email, phone) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this. phone = phone;
+}
+const user1 = new User( id:1, name: 'Ivan', surname: 'Ivanovuch', email: 'example@gmail.com', phone: '1442948564');
+const user2 = new User( id:2, name: 'Andryi', surname: 'Vasylyovuch', email: 'example1@gmail.com', phone: '3442222564');
+const user3 = new User( id:3, name: 'Vasya', surname: 'Mykhailovuch', email: 'example2@gmail.com', phone: '4442900064');
+const user4 = new User( id:4, name: 'Petya', surname: 'Antonovuch', email: 'example3@gmail.com', phone: '4442354564');
 
+const  arrayUsers = [];
+
+arrayUsers.push(user1, user2, user3, user4);
+
+let filter = arrayUsers.filter(value => value.id % 2 === 0);
+
+.
+console.log(filter);
+
+/*
 - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
 
 - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 створити пустий масив, наповнити його 10 об'єктами Client
+ */
+
+console.log(arrayUsers.sort((a,b) => b.id - a.id));
+
+/*
 - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 
 
