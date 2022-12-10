b2ec965c46efff70de345b7436f8a01ed421e236
MishaDrog <drogmisha@gmail.com>
2022-12-10 17:44:05 +0200

    js_hw9

--- lessons9/task9/task9.js
+++ lessons9/task9/task9.js
@@ -185,6 +185,7 @@ for (const item of coursesArray) {
 - додати цей блок в body.
 - клонувати його повністю, та додати клон в body.
 */
+/*
         let div = document.createElement('div');
         div.innerText = 'Hello';
         div.classList.add( 'wrap', 'collapse', 'alpha', 'beta');
@@ -203,7 +204,18 @@ for (const item of coursesArray) {
     ['Main','Products','About us','Contacts']
 Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
 Завдання робити через цикли.
+*/
+/*
+const arr = ['Main','Products','About us','Contacts'];
+let ul = document.getElementsByClassName('menu') [0];
 
+for (const ulElement of arr) {
+     let li = document.createElement('li') ;
+     li.innerText = `${ulElement}`;
+     ul.append('li');
+};
+
+*/
 - Є масив
 let coursesAndDurationArray = [
     {title: 'JavaScript Complex', monthDuration: 5},
@@ -213,10 +225,20 @@ let coursesAndDurationArray = [
     {title: 'FullStack', monthDuration: 7},
     {title: 'Frontend', monthDuration: 4}
 ];
-Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
-Завдання робити через цикли.
+//Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+//Завдання робити через цикли.
+for (const item of coursesAndDurationArray) {
+     let div = document.createElement('div');
 
-- Є масив
+     div.innerText = `${item.title} -- ${item.monthDuration}`;
+     document.body.append(div);
+
+}
+
+
+
+//- Є масив
+
 let coursesAndDurationArray = [
     {title: 'JavaScript Complex', monthDuration: 5},
     {title: 'Java Complex', monthDuration: 6},
