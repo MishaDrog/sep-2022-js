b38ce2d4f9026ab61fe9de1ac46809b7aceba79e
MishaDrog <drogmisha@gmail.com>
2022-12-23 01:01:17 +0200

    js_mini-project

--- mini-project/postdetinfo.js
+++ mini-project/postdetinfo.js
@@ -1,7 +1,6 @@
 
 //  7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
 
-
     let url = new URL(location.href);
     let userPost = url.searchParams.get('post');
     console.log(url);
@@ -9,7 +8,76 @@
     let parse = JSON.parse(userPost);
     console.log(parse);
 
+    const container = document.createElement('div');
+    document.body.append(container);
 
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${parse.id}`)
+    .then( (resp) => resp.json())
+    .then(posts => {
+        for (const item in posts) {
+            const  postInfo = document.createElement('div');
+
+            postInfo.innerText = `${item} ${posts[item]}`;
+            document.body.append(postInfo);
+            if (typeof posts[item] !== 'object') {
+                postInfo.innerText = `${item} - ${posts[item]}`;
+            } else {
+                postInfo.innerText = `${item} :`
+                for (const key in posts[item]) {
+                    const postInnerDiv = document.createElement('div');
+                    if (typeof posts[item][key] !== 'object') {
+                        postInnerDiv.innerText = `${key} - ${posts[item][key]}`;
+                    } else {
+                        postInnerDiv.innerText = `${key}:`;
+                        for (const element in posts[item][key]) {
+                            const htmlDivElement = document.createElement('div');
+                            if (typeof posts[item][key][element] !== 'object') {
+                                htmlDivElement.innerText = `${element} - ${posts[item][key][element]}`;
+                            }
+                            postInnerDiv.append(htmlDivElement);
+                        }
+                    }
+                    postInfo.append(postInnerDiv);
+                }
+            }
+            document.body.append(postInfo);
+        }
+    });
+
+
+// coments button
+
+
+
+    let target = document.querySelector('.target');
+    let button = document.querySelector('.button');
+    button.onclick = function () {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        .then(value => value.json())
+        .then(posts => {
+            for (const post of posts) {
+                let div = document.createElement('div');
+                div.innerText = ` ${post.title} `;
+                let a = document.createElement('a');
+                a.innerText = 'details about post';
+                a.href = `postdetinfo.html?post=${JSON.stringify(post)}`;
+                div.appendChild(a);
+                document.body.appendChild(div);
+
+            }
+
+        });
+
+};
+
+
+
+
+
+
+
+/*
     let target = document.querySelector('.target');
     let button = document.querySelector('.button');
     button.onclick = function() {
@@ -38,6 +106,17 @@
 
     };
 
+*/
+
+
+
+
+
+
+
+
+
+// test code
 
 /*
 fetch(`https://jsonplaceholder.typicode.com/users/${parse.id}/posts`)
@@ -74,11 +153,11 @@ fetch(`https://jsonplaceholder.typicode.com/users/${parse.id}/posts`)
         });
 
 
-
-
-
-
 */
+
+
+
+
 
 
 
